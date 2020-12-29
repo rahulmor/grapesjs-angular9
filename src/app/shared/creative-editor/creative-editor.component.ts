d182e3bafd3019681ec4fd616a8933a88aa19f71
Rahul More <rahulmor@cybage.com>
2020-12-29 19:42:11 +0530

    style manager css changes

--- src/app/shared/creative-editor/creative-editor.component.ts
+++ src/app/shared/creative-editor/creative-editor.component.ts
@@ -18,10 +18,9 @@ export class CreativeEditorComponent implements OnInit,AfterViewInit  {
   panelManager:any;
   customPanel:any;
   canvasHeight:any;
+  stepinfoBox:boolean = true;
   @ViewChild("customid") divView: ElementRef;
   @ViewChild("styletext") textStyle:ElementRef;
-  // @ViewChild("fontfamily") fontfamily:ElementRef;
-  // @ViewChild("panel") panel:ElementRef;
   constructor(private renderer: Renderer2,) {
 
   }
@@ -32,20 +31,11 @@ export class CreativeEditorComponent implements OnInit,AfterViewInit  {
 
   ngOnInit(): void {
     this._editor = this.initializeEditor();
+    this.editor.DomComponents.clear();
     this.editor.getModel().set('dmode', 'absolute');
     this.blockManager = this.editor.BlockManager;
-    // this.panelManager = this.editor.Panels;
-    const deviceManager = this.editor.DeviceManager;
     this.styleManager = this.editor.StyleManager;
-    console.log("commands==",this.editor.Commands.getAll())
-    // deviceManager.add('basic', '300px', {
-    //   height: '250px',
-    //   // At first, GrapesJS tries to localize the name by device id.
-    //   // In case is not found, the `name` property is used (or `id` if name is missing)
-    //   name: 'Basic',
-    //   widthMedia: '250px', // the width that will be used for the CSS media
-    //  });
-     var block1 = this.blockManager.add('image', {
+    var block1 = this.blockManager.add('image', {
        id: 'image',
        label: 'IMAGE',
       //  category: 'Ad Elements',
@@ -119,76 +109,7 @@ export class CreativeEditorComponent implements OnInit,AfterViewInit  {
     this.filtered.push(block2);
     this.filtered.push(block6);
     this.filtered.push(block3);
-    
-    console.log("blocks==",JSON.stringify(this.filtered))
-    // this.filtered = blocks.filter(block => {
-    //   console.log("blocks123==",block.getConfig('id'))
-    //   if(block.get('id') == 'column1'){
-    //     block.prototype.attributes = { class: 'gjs-fonts gjs-f-b1' };
-    //     return block;
-    //   }
-    // });
-    console.log("filtered==",this.filtered);
-    // const panelManager = this.editor.Panels;
-    // console.log("panels=",panelManager.getPanels());
-    // panelManager.getPanels().remove([]);
-    
-    // this.styleManager = this.editor.StyleManager;
-    // const propView = this.styleManager.getSectors();
-    // console.log("stylemanager=",propView);
-    // this.customStyle = propView.filter(block => {
-    //     console.log("sector==",block)
-    //     if(block.get('id') == 'general'){
-    //       return block;
-    //     }
-    //   });
-    //   console.log("customestyle==",this.customStyle);
-      // const parserCss = (css, editor) => {
-      //   const result = [];
-      //   // ... parse the CSS string
-      //     result.push({
-      //       selectors: '.gjs-blocks-cs',
-      //       style: { color: 'red' }
-      //     })
-      //   // ...
-      //   return result; // Result should be ALWAYS an array
-      // };
-      // console.log("parserCss=",parserCss);
-      // this.editor.setCustomParserCss(parserCss);      
-    // var sector = this.styleManager.addSector('mySector',{
-    //   name: 'My sector',
-    //   open: true,
-    //   properties: [{ name: 'My property'}]
-    // }, { at: 0 });
-    // console.log("sector==",JSON.stringify(sector))
-    // this.customStyle = sector;
-    // this.customPanel = this.panelManager.addPanel({
-    //   id: 'myNewPanel',
-    //   visible: true,
-    //   buttons: [
-    //     {
-    //       id: 'myNewButton',
-    //       className: 'someClass',
-    //       command: 'someCommand',
-    //       attributes: { title: 'Some title' },
-    //       active: false,
-    //     },
-    //   ],
-    // });
-    // const canvas = this.editor.Canvas;
-    // this.editor.on('canvas:dragenter', (data) => {
-    //   alert('selected = '+data)
-    //   console.log("data=",data)
-    //   this.editor.StyleManager.addSector('Typography1', {
-    //       name: 'Typography',
-    //       open: true,
-    //       buildProps: ['font-family', 'font-size', 'font-weight', 'letter-spacing', 'color', 'line-height', 'text-align']
-    //     }, { at: 0 });
-    // });
-    // var focus = canvas.hasFocus(function(component){
-    //   return component;
-    //  });
-    //  console.log("canvas=",focus)
+
     this.canvasHeight = 250;
     
     //To set the base style of the wrapper  
@@ -204,11 +125,11 @@ export class CreativeEditorComponent implements OnInit,AfterViewInit  {
     //Event when a component is selected
     this.editor.on('component:selected', (component) => {
       // alert('selected')
-      this.editor.StyleManager.addSector('Typography', {
-          name: 'Typography',
+      this.editor.StyleManager.addSector('TextStyle', {
+          name: 'TEXT STYLE',
           open: true,
           buildProps: ['font-family', 'font-size', 'font-weight', 'letter-spacing', 'color', 'line-height', 'text-align']
-        }, { at: 0 });
+        }, { at: 1 });
     });
 
       //Drag Event of component 
@@ -240,59 +161,54 @@ export class CreativeEditorComponent implements OnInit,AfterViewInit  {
         domElement.left = '0px'
         this.editor.getSelected().setStyle({...domElement});
       }
-      // Commented this as updated logic above for the same
-      /*if(wrapperHeight > this.canvasHeight) {
-        let allComponents  = this.editor.getComponents();
-        let componentsToBeRemoved = allComponents.filter(
-          componentModel => (
-            componentModel.attributes['tagName']));
-        const lastComponentIndex = componentsToBeRemoved.length - 1;
-        componentsToBeRemoved = componentsToBeRemoved[lastComponentIndex];
-        componentsToBeRemoved.collection.remove(componentsToBeRemoved);
-        alert("Height Exceeded");
-      }*/
     });
     
+  }
+  stepInfoClose(){
+    console.log("closed");
+    this.stepinfoBox = false;
+  }
+  checkCheckBoxvalue(event){
+    console.log(event.target.checked);
   }
   onClickAlignLeft(){
     const component = this.editor.getSelected();
     console.log("component=",component);
-    component && component.addAttributes({ style: {align:"left"}});
+    component && component.addAttributes({ style: {"text-align":"left"}});
   }
   onClickAlignCenter(){
     const component = this.editor.getSelected();
     console.log("component=",component);
-    component && component.addAttributes({ style: {align:"center"}});
+    component && component.addAttributes({ style: {"text-align":"center"}});
   }
   onClickAlignRight(){
     const component = this.editor.getSelected();
     console.log("component=",component);
-    component && component.addAttributes({ style: {align:"right"}});
+    component && component.addAttributes({ style: {"text-align":"right"}});
   }
   changeHeight(e){
     console.log("height==",e.target.value)
     const component = this.editor.getSelected();
     console.log("component=",component);
-    component && component.addAttributes({ style: {height:e.target.value+"px"}});
+    component && component.addAttributes({ style: {"height":e.target.value+"px","margin":"50px"}});
+  }
+  changeWidth(e){
+    console.log("width==",e.target.value)
+    const component = this.editor.getSelected();
+    console.log("component=",component);
+    component && component.addAttributes({ style: {width:e.target.value+"px"}});
   }
   ngAfterViewInit(){
     const newBlocksEl = this.blockManager.render(this.filtered, { external: true });
     console.log("newblock==",newBlocksEl);
-    // document.getElementById('customid').appendChild(newBlocksEl);
-    console.log(this.divView);
-    // this.divView.nativeElement.innerHTML = newBlocksEl;
     this.renderer.appendChild(this.divView.nativeElement, newBlocksEl);
 
     const newSector = this.styleManager.render(this.customStyle, { external: true });
     console.log("newSector==",newSector);
     this.renderer.appendChild(this.textStyle.nativeElement, newSector);
-    // const newPanel = this.panelManager.render(this.customPanel, { external: true });
-    // console.log("newPanel==",newPanel);
-    // this.renderer.appendChild(this.panel.nativeElement, newPanel);
     
   }
   private initializeEditor(): any {
-    console.dir(window);
     return grapesjs.init({
       container: '#gjs',
       autorender: true,
@@ -302,89 +218,30 @@ export class CreativeEditorComponent implements OnInit,AfterViewInit  {
       style: '',
       panels: { defaults: [] },
       // plugins: ['gjs-blocks-basic'],
-      // layerManager: {
-      //   appendTo: '#layers-container'
-      // },
-      // blockManager: {
-      //   appendTo: '#blocks',
-        // blocks: [
-        //   {
-        //     id: 'section', // id is mandatory
-        //     label: '<b>Section</b>', // You can use HTML/SVG inside labels
-        //     attributes: { class:'gjs-block-section' },
-        //     content: `<section>
-        //       <h1>This is a simple title</h1>
-        //       <div>This is just a Lorem text: Lorem ipsum dolor sit amet</div>
-        //     </section>`,
-        //   }, {
-        //     id: 'text',
-        //     label: 'Creative 4 Live',
-        //     content: '<div data-gjs-type="text" style="color:red;font-size:14px;">Creative 4 Live</div>',
-        //   }, {
-        //     id: 'image',
-        //     label: 'Image',
-        //     // Select the component once it's dropped
-        //     select: true,
-        //     // You can pass components as a JSON instead of a simple HTML string,
-        //     // in this case we also use a defined component type `image`
-        //     content: { type: 'image' },
-        //     // This triggers `active` event on dropped components and the `image`
-        //     // reacts by opening the AssetManager
-        //     activate: true,
-        //   },
-        //   {
-        //     id: 'image1',
-        //     label: 'Image1',
-        //     // Select the component once it's dropped
-        //     select: true,
-        //     // You can pass components as a JSON instead of a simple HTML string,
-        //     // in this case we also use a defined component type `image`
-        //     content: { type: 'image' },
-        //     // This triggers `active` event on dropped components and the `image`
-        //     // reacts by opening the AssetManager
-        //     activate: true,
-        //   },
-        //   {
-        //     id: 'image2',
-        //     label: 'Image2',
-        //     // Select the component once it's dropped
-        //     select: true,
-        //     // You can pass components as a JSON instead of a simple HTML string,
-        //     // in this case we also use a defined component type `image`
-        //     content: { type: 'image' },
-        //     // This triggers `active` event on dropped components and the `image`
-        //     // reacts by opening the AssetManager
-        //     activate: true,
-        //   }
-        // ]
-      // },
-      // traitManager: {
-      //   appendTo: '#traits-container',
-      // },
       styleManager: {
-        appendTo: '#style-manager-container',
+      //   appendTo: '#style-manager-container',
         sectors: [{
           id:"general",
           name: 'General',
           open: true,
-          buildProps: ['float', 'display', 'position', 'width', 'height', 'top', 'left','transform'],
+          buildProps: ['float', 'position', 'width', 'height', 'top', 'left','transform'],
           properties:[
             {
                 property:'transform',
                 properties:[
                     {
-                        name:'Translate Y',
-                        property:'transform-translate-y',
-                        functionName: 'translateY',
+                        name:'Rotate Y',
+                        property:'transform-rotate-y',
+                        functionName: 'rotateY',
                         defaults: 0,
                         type: 'integer',
                         units: ['px', '%'],
                         unit: 'px'
                     },
                     {
-                        name:'Translate X',
-                        property:'transform-translate-x',
-                        functionName: 'translateX',
+                        name:'Rotate X',
+                        property:'transform-rotate-x',
+                        functionName: 'rotateX',
                         defaults: 0,
                         type: 'integer',
                         units: ['px', '%'],
@@ -394,49 +251,13 @@ export class CreativeEditorComponent implements OnInit,AfterViewInit  {
             }
         ]
         },
-        //  {
-        //   name: 'Dimension',
-        //   open: false,
-        //   buildProps: ['width', 'height', 'max-width', 'min-height', 'margin', 'padding']
-        // },
-         {
-          name: 'Text Style',
-          open: true,
-          buildProps: ['font-family','font-weight', 'font-size','line-height', 'letter-spacing','text-align', 'color',  ]
-        }
-        // , {
-        //   name: 'Decorations',
-        //   open: false,
-        //   buildProps: ['border-radius-c', 'background-color', 'border-radius', 'border', 'box-shadow', 'background'],
-        // }
-        // , {
-        //   name: 'Extra',
-        //   open: false,
-        //   buildProps: ['opacity', 'transition', 'perspective', 'transform'],
-        //   properties: [{
-        //     type: 'slider',
-        //     property: 'opacity',
-        //     defaults: 1,
-        //     step: 0.01,
-        //     max: 1,
-        //     min: 0,
-        //   }]
+      //   {
+      //     name: 'Text Style',
+      //     open: true,
+      //     buildProps: ['font-family','font-weight', 'font-size','line-height', 'letter-spacing','text-align', 'color',  ]
         // }
       ],
       },
-      // plugins: ['gjs-preset-webpage'],
-      // pluginsOpts: {
-      //   'gjs-preset-webpage': {
-      //     navbarOpts: false,
-      //     countdownOpts: false,
-      //     formsOpts: false,
-      //     blocksBasicOpts: {
-      //       blocks: ['link-block', 'quote', 'image', 'video', 'text', 'column1', 'column2', 'column3','table'],
-      //       flexGrid: false,
-      //       stylePrefix: 'lala-'
-      //     }
-      //   }
-      // },
       assetManager: {
         uploadText: 'Add image through link or upload image',
         modalTitle: 'Select Image',
