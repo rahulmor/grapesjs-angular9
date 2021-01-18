0133961c745e1331746ebff7b01fee1d286b4a1b
Rahul More <rahulmor@cybage.com>
2021-01-18 23:20:56 +0530

    image drag n drop is done

--- src/app/shared/plugins/popup-plugin.ts
+++ src/app/shared/plugins/popup-plugin.ts
@@ -2,99 +2,15 @@ import grapesjs from 'grapesjs';
 export default grapesjs.plugins.add(
   'plista-adbuilder-preset',
   (editor, options) => {
-    //Alternate solution as creating type
-    const domc = editor.DomComponents;
-    const defaultType = domc.getType('default');
-    const defaultModel = defaultType.model;
-    const defaultView = defaultType.view;
-
-    domc.addType('test-type', {
-      model: defaultModel.extend({
-        defaults: Object.assign({}, defaultModel.prototype.defaults, {
-          type: 'test-type',
-          id: 'popup',
-          droppable: false,
-          resizable: true,
-          tagName: 'popup',
-        }),
-        getAttrToHTML: function () {
-          var attr = defaultType.model.prototype.getAttrToHTML.apply(
-            this,
-            arguments
-          );
-          delete attr.onmousedown;
-          var testmarker = this.get('testmarker');
-          if (testmarker) attr.testmarker = testmarker;
-          console.log(attr);
-          return attr;
-        },
-      }),
-      view: defaultType.view.extend({
-        events: {
-          click: 'handleClick',
-        },
-        createContent() {
-          const content = document.createElement('div');
-          // content.style = 'position: relative';
-          content.innerHTML = `
-                    <div class="container"> 
-                      <form>
-                          <div>
-                              <label>URL: </label>
-                               <input type="text" id="url" placeholder="http://test-data/" name="url">
-                          </div>
-                          <div>
-                              <label>Identity: </label>
-                               <input type="text" id="address" placeholder="Enter Identity Address" name="address">
-                          </div>
-                      </form>
-                    </div>
-        <button style="
-          position: absolute;
-          top: 0; right: 0;
-          background-color: #fff;
-          font-size: 1rem;
-          border-radius: 3px;
-          border: none;
-          padding: 10px 20px;
-          cursor: pointer
-        ">
-             Submit
-        </botton>
-      `;
-
-          return content;
-        },
-        handleClick: function (e) {
-          var modal = editor.Modal;
-          var content = this.createContent();
-          var btn = content.children.length === 2 && content.children[1];
-          btn.addEventListener('click', this.applyChanges);
-          modal.open({
-            title: '<br>Create Identity<br>',
-            content: content,
-          });
-        },
-        applyChanges() {
-          // DO YOUR AJAX STUFF HERE
-          console.log('applychanges==');
-          editor.setComponents('<div class="cls">New component</div>');
-        },
-      }),
-    });
-    //Main solution overriding existing asset manager UI 
     editor.Commands.add('open-assets', {
       run(editor, sender, opts) {
         const assettarget = opts.target;
         // code to open your own modal goes here.
         openModal(assettarget);
-//         const assettarget = opts.target;
-// assetTarget.set("src", image.url);
       },
     });
     function openModal(assettarget) {
       var modalContent = document.createElement('div');
-      // modalContent.setAttribute("style", `.active { background-color: #7a808d; }`);
       modalContent.innerHTML = `<div>
       <ul class="nav nav-tabs image-upload-modal" role="tablist">
         <li role="presentation" class="tab-menu active" id="default-image" >
@@ -113,59 +29,75 @@ export default grapesjs.plugins.add(
       <!-- Tab panes -->
       <div class="tab-content">
         <div role="tabpanel" class="tab-pane active" id="image">
-          <div class="dropzone-wrapper">
-          <div class="dropzone-desc">
-                <i class="fas fa-upload"></i>
-                <p>Choose an image file or drag it here.</p>
+        <form id="file-upload-form" class="uploader">
+        <div style="height:0px;overflow:hidden">
+                  <input type="file" id="fileInput" name="fileInput" accept="image/*"/>
               </div>
-              <input type="file" name="img_logo" class="dropzone"></div>
+          <div class="dropzone-wrapper" id="file-drag" draggable="true">
+          <div class="dropzone-desc">
+                <span class="upload-icon"><i class="far fa-arrow-to-top"></i></span>
+                <p class="upload-text">DRAG AND DROP IMAGE</p>
+                
+              <button type="button" class="select-image" id="image-upload">SELECT IMAGE</button>
+              </div>
+           </div>
+        </form>
         </div>
-        <div role="tabpanel" class="tab-pane" id="instagram">Instagram</div>
-        <div role="tabpanel" class="tab-pane" id="unsplash">Unsplash</div>
-        <div role="tabpanel" class="tab-pane" id="shutterstock">Shutterstock</div>
+        <div role="tabpanel" class="tab-pane" id="instagram">In progress</div>
+        <div role="tabpanel" class="tab-pane" id="unsplash">In progress</div>
+        <div role="tabpanel" class="tab-pane" id="shutterstock">In progress</div>
       </div>
     </div>
     `;
-    const tab_menu = modalContent.querySelectorAll('.image-upload-modal > li');
-    const tab = modalContent.querySelector('.tab-menu')
-    tab.classList.remove('active');
-    tab_menu[0].classList.add('active');
-    tab_menu.forEach(currenttab => {
-      let curTab = currenttab.querySelectorAll('a');
-      currenttab.addEventListener("click", event => {
-        tab_menu.forEach(removeClassEle => {
-          removeClassEle.classList.remove('active');
+      const tab_menu = modalContent.querySelectorAll(
+        '.image-upload-modal > li'
+      );
+      const tab = modalContent.querySelector('.tab-menu');
+      tab.classList.remove('active');
+      tab_menu[0].classList.add('active');
+      tab_menu.forEach((currenttab) => {
+        // let curTab = currenttab.querySelectorAll('a');
+        currenttab.addEventListener('click', (event) => {
+          tab_menu.forEach((removeClassEle) => {
+            removeClassEle.classList.remove('active');
+          });
+          currenttab.classList.add('active');
         });
-        currenttab.classList.add('active');
       });
-    });
-    
-      // var editorTextArea = document.createElement('textarea');
 
-      // var saveButton = document.createElement('button');
-      // saveButton.innerHTML = 'Save';
-      // saveButton.className = 'gjs-btn-prim';
-      // // saveButton.style = 'margin-top: 8px;';
-      // saveButton.onclick = function () {
-      //   // var content = codeViewer.editor.getValue();
-      //   // editor.getSelected().set('content', content);
-      //   editor.Modal.close();
-      // };
+      var fileSelect = modalContent.querySelector('#fileInput');
+      modalContent
+        .querySelector('#image-upload')
+        .addEventListener('click', (event) => {
+          document.getElementById('fileInput').click();
+        });
+      fileSelect.addEventListener(
+        'change',
+        function (e) {
+          var tmppath = URL.createObjectURL(this.files[0]);
+          assettarget.set('src', tmppath);
+          editor.Modal.setContent(modalContent).close();
+        },
+        false
+      );
 
-      // modalContent.appendChild(editorTextArea);
-      // modalContent.appendChild(saveButton);
-
-      // // codeViewer.init(editorTextArea);
-
-      // var htmlContent = document.createElement('div');
-      // htmlContent.innerHTML = editor.getSelected().toHTML();
-      // htmlContent = htmlContent.firstChild.innerHTML;
-      // codeViewer.setContent(htmlContent);
-
+      var dropArea = modalContent.querySelector('#file-drag');
+      ['dragenter', 'dragover', 'dragleave', 'drop'].forEach((eventName) => {
+        dropArea.addEventListener(eventName, preventDefaults, false);
+      });
+      function preventDefaults(e) {
+        e.preventDefault();
+        e.stopPropagation();
+      }
+      dropArea.addEventListener('drop', handleDrop, false);
+      function handleDrop(e) {
+        var tmppath = URL.createObjectURL(e.dataTransfer.files[0]);
+        assettarget.set('src', tmppath);
+        editor.Modal.setContent(modalContent).close();
+      }
       editor.Modal.setContent(modalContent).open();
     }
     // Get the Asset Manager module first
-
     editor.on('block:drag:stop', (model) => {
       const modal = editor.Modal;
       /**
