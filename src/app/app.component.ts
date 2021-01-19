c45c9d694e15113315ad69150ea9d32f03b5b3f4
harshabh18 <69294797+harshabh18@users.noreply.github.com>
2021-01-19 11:28:49 +0530

    Ad builder dev harsha (#22)
    * adding font awsome 5 pro
    
    * added review change
    
    * added review change
    
    * added review change and take pull from ad-builder-dev
    
    * added buttons
    
    * send back and to front, delete component and canvas size
    
    * buttons added and functionality
    
    * Send back and set front functionality done
    
    * client feedback and changes in code
    
    * client feedback and changes in code for copy the component
    
    * update code as per Review comment
    
    * update code as per Review comment
    
    * unite testing runs the basics for all the components
    
    * changes for testing
    
    * add css
    
    * add css
    
    * add css
    
    * Login functionality check
    
    * Login functionality check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LoginService } from './services/login.service';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'grapes-angular';
-  constructor() {
-
-  }
-  ngOnInit() {
+  constructor(public loginService: LoginService, private router: Router) {
     
   }
-  
+  ngOnInit() {
+    setInterval(()=>{ 
+      this.loginService.login_check().subscribe(resp =>{
+        if(resp.data === false){
+          window.location.href = 'https://login-test.plista.com/de/';
+        }
+      });
+     }, 15000);
+  }
 }
